7acaa576482158bbe61d81caa34e1d53732aeb53
Prashant pal <palprashant156@gmail.com>
2025-05-31 09:27:41 +0530

    uuddddcode

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['axios'],
-      output: {
-        globals: {
-          axios: 'axios'
-        }
-      }
+    commonjsOptions: {
+      include: [/axios/, /node_modules/]
     }
   },
   resolve: {
